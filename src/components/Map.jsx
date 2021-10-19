8dd9d4b91a9533c7cd8ad54021a5209a86ebefb1
cegarciav <cegarcia2@uc.cl>
2021-10-19 00:29:54 -0300

    Setup Leaflet in the project

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from 'react-leaflet';
 
 function Map() {
   return (
     <article className="col-6 px-2 py-2">
       <h1>Mapa en vivo flota de camiones</h1>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </article>
   );
 }
