78243b0e545eb83ce8843d1398299a7493bc6389
cegarciav <cegarcia2@uc.cl>
2021-10-20 19:25:02 -0300

    Create http service to communicate with reverse geocoding api

---
+++ src/httpService.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const apiOrigin = process.env.REACT_APP_ADDRESSES_NAMES_API;
+
+const apiRequester = axios.create({
+  baseURL: apiOrigin,
+});
+
+const errorGenerator = (e) => {
+  let errorResponse = null;
+  if (e.response) {
+    errorResponse = {
+      errors: e.response.data.errors,
+      type: 'response',
+      statusCode: e.response.status,
+    };
+  } else {
+    errorResponse = {
+      type: 'unexpected',
+      message: 'Algo salió mal. Por favor, revisa tu conexión o intenta ingresar más tarde',
+    };
+  }
+  return errorResponse;
+};
+
+async function apiGet(path, params) {
+  try {
+    if (!apiOrigin) throw Error('Los nombres de origen y destino de los camiones no serán mostrados, inténtelo más tarde');
+    const url = new URL(`${apiOrigin}${path}`);
+    if (params) {
+      Object.keys(params)
+        .forEach((key) => url.searchParams.append(key, params[key]));
+    }
+    const response = await apiRequester.get(url);
+    return {
+      data: response.data,
+      statusCode: response.status,
+    };
+  } catch (e) {
+    return errorGenerator(e);
+  }
+}
+
+export default apiGet;
