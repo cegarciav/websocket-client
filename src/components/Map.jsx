4ab39728b7f588ceb738044ece9e63ca22c6076a
cegarciav <cegarcia2@uc.cl>
2021-10-20 02:11:15 -0300

    Display trucks in map in real time

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   MapContainer,
   TileLayer,
-  Marker,
-  Popup,
 } from 'react-leaflet';
+import TruckMarker from './TruckMarker';
+import trucksSocket from '../socketService';
 
 function Map() {
+  const [trucksData, setTrucksData] = useState([]);
+  const [routes, setRoutes] = useState({});
+  const [lastCode, setLastCode] = useState(null);
+  const [lastPosition, setLastPosition] = useState(null);
+  const [centre, setCentre] = useState(null);
+  const [isCentred, setIsCentred] = useState(false);
+
+  useEffect(() => {
+    trucksSocket.emit('TRUCKS');
+    trucksSocket.on('TRUCKS', (trucksList) => {
+      setTrucksData(trucksList);
+    });
+  }, []);
+
+  useEffect(() => {
+    trucksSocket.on('POSITION', (newPosition) => {
+      // Update routes
+      setRoutes((prevRoutes) => {
+        const routeCodes = Object.keys(prevRoutes);
+        let newRoutes = {};
+
+        if (routeCodes.includes(newPosition.code)) {
+          newRoutes = {
+            ...prevRoutes,
+            [newPosition.code]: [...prevRoutes[newPosition.code], newPosition.position],
+          };
+        } else {
+          newRoutes = {
+            ...prevRoutes,
+            [newPosition.code]: [newPosition.position],
+          };
+        }
+        return newRoutes;
+      });
+
+      setLastCode(newPosition.code);
+      setLastPosition(newPosition.position);
+    });
+  }, []);
+
+  useEffect(() => {
+    // Update list of trucks if necessary
+    const truckCodes = trucksData.map((truck) => truck.code);
+    if (!truckCodes.includes(lastCode)) {
+      trucksSocket.emit('TRUCKS');
+    }
+  }, [lastCode]);
+
+  useEffect(() => {
+    // Update centre if necessary
+    if (!isCentred && lastPosition) {
+      setCentre(lastPosition);
+      setIsCentred(true);
+    }
+  }, [lastPosition]);
+
   return (
     <article className="col-6 px-2 py-2">
       <h1>Mapa en vivo flota de camiones</h1>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      {centre
+        && (
+          <MapContainer center={centre} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+              Object
+                .keys(routes)
+                .map((truckCode) => routes[truckCode][routes[truckCode].length - 1])
+                .map((pos) => <TruckMarker popupInfo="Random text" position={pos} key={`${pos[0]}-${pos[1]}`} />)
+            }
+          </MapContainer>
+        )}
     </article>
   );
 }
